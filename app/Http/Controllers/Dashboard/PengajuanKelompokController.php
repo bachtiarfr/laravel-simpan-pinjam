b0a7cf83f387b24fcd06e1082545cdaed9576b9d
gilangsukmaw <gilangsukmaw07@gmail.com>
2024-03-10 15:03:55 +0700

    update pengajuan kelompok if rejected

--- app/Http/Controllers/Dashboard/PengajuanKelompokController.php
+++ app/Http/Controllers/Dashboard/PengajuanKelompokController.php
@@ -17,14 +17,49 @@ class PengajuanKelompokController extends Controller
         $jenis_kelompok = JenisKelompok::all();
         $kelompok = Kelompok::select('kelompok.id', 'kelompok.approval_status','kelompok.approval_reason','kelompok.no_ktp', 'kelompok.nama_kelompok', 'kelompok.alamat', 'kelompok.telepon', 'kelompok.deleted_at', 'jenis_kelompok.name AS jenis_kelompok', 'kelompok.approval_status')->join('jenis_kelompok', 'jenis_kelompok.id', '=', 'kelompok.jenis_kelompok_id')->where('user_id','=',Auth::user()->id)->first();
         //  Kelompok::all()->where('user_id','=',Auth::user()->id)->first();
-        return view('kelompok.pengajuan_kelompok', compact('jenis_kelompok','kelompok'));
+        $approval_status = false ;
+        if ($kelompok != null) {
+            $approval_status = $kelompok->approval_status;
+        }
+        
+        return view('kelompok.pengajuan_kelompok', compact('jenis_kelompok','kelompok', 'approval_status'));
     }
 
     public function store(Request $request)
     {
+        $cek_kelompok = Kelompok::select('*')->where('user_id','=',Auth::user()->id)->first();
+
+        if($cek_kelompok != null){
+            $request->validate([
+            'file' => 'required|mimes:doc,pdf,docx,zip|max:2048',
+            'no_ktp' => 'required|digits:16',
+            'nama_kelompok' => 'required|string|max:100',
+            'alamat' => 'required|max:200',
+            'telepon' => 'required|max:12',
+            'jenis_kelompok_id' => 'required',
+            ]);
+
+             $fileName = time().'.'.$request->file->extension();  
+             $request->file->move(public_path('docs'), $fileName);
+
+            
+            $data = [
+                'no_ktp' => $request->no_ktp,
+                'nama_kelompok' => $request->nama_kelompok,
+                'approval_status' => 'waiting',
+                'approval_reason' => '',
+                'alamat' => $request->alamat,
+                'telepon' => $request->telepon,
+                'jenis_kelompok_id' => $request->jenis_kelompok_id,
+                'document_administrations' => $fileName,
+            ];
+
+            $cek_kelompok->update($data);
+            return redirect()->route('dashboard.anggota')->with(['status' => 'Data kelompok berhasil di ajukan']);
+        }        
 
         $request->validate([
-            'file' => 'required|mimes:png,jpg,jpeg|max:2048',
+            'file' => 'required|mimes:doc,pdf,docx,zip|max:2048',
             'no_ktp' => 'required|unique:kelompok|digits:16',
             'nama_kelompok' => 'required|string|max:100',
             'alamat' => 'required|max:200',
--- resources/views/kelompok/pengajuan_kelompok.blade.php
+++ resources/views/kelompok/pengajuan_kelompok.blade.php
@@ -29,19 +29,19 @@
 
 </div>
 
-@if ($kelompok != null && $kelompok->approval_status == 'waiting')
+@if ($kelompok != null && $approval_status == 'waiting')
 <div class="alert alert-secondary text-white" role="alert">
     Anda telah melakukan pengajuan, silahkan tunggu admin untuk memverifikasi pengajuan anda
 </div>
 @endif
 
-@if ($kelompok != null && $kelompok->approval_status == 'approved')
+@if ($kelompok != null && $approval_status == 'approved')
 <div class="alert alert-success text-white" role="alert">
     Pengajuan kelompok anda telah di verifikasi oleh admin
 </div>
 @endif
 
-@if ($kelompok != null && $kelompok->approval_status == 'reject')
+@if ($kelompok != null && $approval_status == 'reject')
 <div class="alert alert-danger text-white" role="alert">
     Pengajuan kelompok anda di tolak oleh admin karena:
     {{$kelompok->approval_reason}}
@@ -61,9 +61,8 @@
                                 <input type="text"
                                     class="form-control {{ $errors->first('no_ktp') ? 'is-invalid' : '' }}" id="no_ktp"
                                     name="no_ktp" value="{{  $kelompok != null ? $kelompok->no_ktp : old('no_ktp')  }}"
-                                    {{$kelompok !=null && $kelompok->approval_status == 'waiting' ||
-                                $kelompok->approval_status == 'approved' ? 'disabled' : null
-                                }}>
+                                    {{$kelompok !=null && $approval_status=='waiting' || $approval_status=='approved'
+                                    ? 'disabled' : null }}>
                                 <div class="invalid-feedback">
                                     {{$errors->first('no_ktp')}}
                                 </div>
@@ -74,9 +73,8 @@
                                     class="form-control {{ $errors->first('nama_kelompok') ? 'is-invalid' : '' }}"
                                     id="nama_kelompok" name="nama_kelompok"
                                     value="{{  $kelompok != null ? $kelompok->nama_kelompok : old('nama_kelompok')  }}"
-                                    {{$kelompok !=null && $kelompok->approval_status == 'waiting' ||
-                                $kelompok->approval_status == 'approved' ? 'disabled' : null
-                                }}>
+                                    {{$kelompok !=null && $approval_status=='waiting' || $approval_status=='approved'
+                                    ? 'disabled' : null }}>
                                 <div class="invalid-feedback">
                                     {{$errors->first('nama_kelompok')}}
                                 </div>
@@ -84,9 +82,8 @@
                             <div class="my-3">
                                 <label for="textarea">Alamat</label>
                                 <textarea class="form-control {{ $errors->first('alamat') ? 'is-invalid' : '' }}"
-                                    placeholder="Tulis alamat lengkap..." id="alamat" name="alamat" rows="4" {{$kelompok
-                                    !=null ? 'disabled' : null
-                                    }}>{{ $kelompok != null ? $kelompok->alamat : old('alamat')}}</textarea>
+                                    placeholder="Tulis alamat lengkap..." id="alamat" name="alamat"
+                                    rows="4">{{ $kelompok != null ? $kelompok->alamat : old('alamat')}}</textarea>
                                 <div class="invalid-feedback">
                                     {{$errors->first('alamat')}}
                                 </div>
@@ -94,17 +91,14 @@
                         </div>
                         <div class="col-lg-5 col-sm-6">
                             <div class="mb-3">
-                                <label for="telepon">Telepon</label>
-                                <div class="input-group">
-                                    <span class="input-group-text"><span class="fas fa-phone"></span></span>
-                                    <input type="text"
-                                        class="form-control {{ $errors->first('telepon') ? 'is-invalid' : '' }}"
-                                        id="telepon" name="telepon" {{$kelompok !=null && $kelompok->approval_status ==
-                                    'waiting' || $kelompok->approval_status == 'approved' ? 'disabled' : null }}
+                                <label for="nama_kelompok">Telepon</label>
+                                <input type="text"
+                                    class="form-control {{ $errors->first('telepon') ? 'is-invalid' : '' }}"
+                                    id="telepon" name="telepon" {{$kelompok !=null && $approval_status=='waiting' ||
+                                    $approval_status=='approved' ? 'disabled' : null }}
                                     value="{{ $kelompok != null ? $kelompok->telepon : old('telepon')}}">
-                                    <div class="invalid-feedback">
-                                        {{$errors->first('telepon')}}
-                                    </div>
+                                <div class="invalid-feedback">
+                                    {{$errors->first('telepon')}}
                                 </div>
                             </div>
 
@@ -116,11 +110,10 @@
                                 <select
                                     class="form-select {{ $errors->first('jenis_kelompok_id') ? 'is-invalid' : '' }}"
                                     name="jenis_kelompok_id" id="jenis_kelompok_id" {{$kelompok !=null &&
-                                    $kelompok->approval_status == 'waiting' || $kelompok->approval_status == 'approved'
-                                    ? 'disabled' :
-                                    null }}>
+                                    $approval_status=='waiting' || $approval_status=='approved' ? 'disabled' : null }}>
                                     <option value="{{ $kelompok != null ? $kelompok->jenis_kelompok : ''}}"
                                         selected="selected">
+                                        {{ $kelompok != null ? $kelompok->jenis_kelompok : ''}}
                                     </option>
                                     @foreach ($jenis_kelompok as $jk)
                                     <option value="{{ $jk->id }}">{{ $jk->name }}</option>
@@ -134,8 +127,7 @@
                             <div class="mb-4">
                                 <label for="file">Syarat Administrasi</label>
                                 <input type="file" name="file" id="file" {{$kelompok !=null &&
-                                    $kelompok->approval_status == 'waiting' || $kelompok->approval_status == 'approved'
-                                ? 'disabled' : null }}>
+                                    $approval_status=='waiting' || $approval_status=='approved' ? 'disabled' : null }}>
                                 <div class="invalid-feedback">
                                     {{$errors->first('file')}}
                                 </div>
@@ -144,8 +136,8 @@
                             <div class="mb-3">
                                 {{-- <input type="submit" value="Simpan"> --}}
                                 <button type="submit" class="btn btn-secondary text-dark" {{$kelompok !=null &&
-                                    $kelompok->approval_status == 'waiting' || $kelompok->approval_status == 'approved'
-                                    ? 'disabled' : null }}>Simpan</button>
+                                    $approval_status=='waiting' || $approval_status=='approved' ? 'disabled' : null
+                                    }}>Simpan</button>
                             </div>
                         </div>
                     </div>
